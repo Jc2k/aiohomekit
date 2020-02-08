6412b265adf697a1ea4b1342d8d61c4d4e93d52f
John Carr <john.carr@unrouted.co.uk>
2020-02-08 20:49:15 +0000

    De-unittest-ification

--- tests/test_crypto_srp.py
+++ tests/test_crypto_srp.py
@@ -14,34 +14,31 @@
 # limitations under the License.
 #
 
-import unittest
-
 from aiohomekit.crypto.srp import SrpClient, SrpServer
 
 
-class TestSrp(unittest.TestCase):
-    def test_1(self):
-        # step M1
+def test_1():
+    # step M1
 
-        # step M2
-        setup_code = "123-45-678"  # transmitted on second channel
-        server = SrpServer("Pair-Setup", setup_code)
-        server_pub_key = server.get_public_key()
-        server_salt = server.get_salt()
+    # step M2
+    setup_code = "123-45-678"  # transmitted on second channel
+    server = SrpServer("Pair-Setup", setup_code)
+    server_pub_key = server.get_public_key()
+    server_salt = server.get_salt()
 
-        # step M3
-        client = SrpClient("Pair-Setup", setup_code)
-        client.set_salt(server_salt)
-        client.set_server_public_key(server_pub_key)
+    # step M3
+    client = SrpClient("Pair-Setup", setup_code)
+    client.set_salt(server_salt)
+    client.set_server_public_key(server_pub_key)
 
-        client_pub_key = client.get_public_key()
-        clients_proof = client.get_proof()
+    client_pub_key = client.get_public_key()
+    clients_proof = client.get_proof()
 
-        # step M4
-        server.set_client_public_key(client_pub_key)
-        server.get_shared_secret()
-        self.assertTrue(server.verify_clients_proof(clients_proof))
-        servers_proof = server.get_proof(clients_proof)
+    # step M4
+    server.set_client_public_key(client_pub_key)
+    server.get_shared_secret()
+    assert server.verify_clients_proof(clients_proof) is True
+    servers_proof = server.get_proof(clients_proof)
 
-        # step M5
-        self.assertTrue(client.verify_servers_proof(servers_proof))
+    # step M5
+    assert client.verify_servers_proof(servers_proof) is True
