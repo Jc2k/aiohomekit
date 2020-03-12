709301323274de09e25d55e53ec738361ffe61b7
John Carr <john.carr@unrouted.co.uk>
2020-03-12 12:53:35 +0000

    Have clamp work on a char object instead of a dict

--- aiohomekit/utils.py
+++ aiohomekit/utils.py
@@ -1,25 +1,27 @@
 import enum
 
+from aiohomekit.model.characteristics import Characteristic
 
-def clamp_enum_to_char(all_valid_values: enum.IntEnum, char):
+
+def clamp_enum_to_char(all_valid_values: enum.IntEnum, char: Characteristic):
     """Clamp possible values of an enum to restrictions imposed by a manufacturer."""
     valid_values = set(all_valid_values)
 
-    if "minValue" in char:
+    if char.minValue:
         valid_values = {
             target_state
             for target_state in valid_values
-            if target_state >= char["minValue"]
+            if target_state >= char.minValue
         }
 
-    if "maxValue" in char:
+    if char.maxValue:
         valid_values = {
             target_state
             for target_state in valid_values
-            if target_state <= char["maxValue"]
+            if target_state <= char.maxValue
         }
 
-    if "valid-values" in char:
-        valid_values = valid_values.intersection(set(char["valid-values"]))
+    if char.valid_values:
+        valid_values = valid_values.intersection(set(char.valid_values))
 
     return valid_values
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,20 +1,33 @@
-from aiohomekit.model.characteristics import RemoteKeyValues
+from aiohomekit.model import Accessory
+from aiohomekit.model.characteristics import CharacteristicsTypes, RemoteKeyValues
+from aiohomekit.model.services import ServicesTypes
 from aiohomekit.utils import clamp_enum_to_char
 
 
 def test_clamp_enum_valid_vals():
-    valid_vals = clamp_enum_to_char(
-        RemoteKeyValues, {"valid-values": [RemoteKeyValues.PLAY_PAUSE]}
+    a = Accessory()
+    tv_service = a.add_service(service_type=ServicesTypes.TELEVISION)
+    char = tv_service.add_char(
+        CharacteristicsTypes.REMOTE_KEY,
+        valid_values=[RemoteKeyValues.PLAY_PAUSE],
+        min_value=None,
+        max_value=None,
     )
+
+    valid_vals = clamp_enum_to_char(RemoteKeyValues, char)
     assert valid_vals == {RemoteKeyValues.PLAY_PAUSE}
 
 
 def test_clamp_enum_min_max():
-    valid_vals = clamp_enum_to_char(
-        RemoteKeyValues,
-        {
-            "minValue": RemoteKeyValues.PLAY_PAUSE,
-            "maxValue": RemoteKeyValues.PLAY_PAUSE,
-        },
+    a = Accessory()
+    tv_service = a.add_service(service_type=ServicesTypes.TELEVISION)
+    char = tv_service.add_char(
+        CharacteristicsTypes.REMOTE_KEY,
+        valid_values=None,
+        min_value=RemoteKeyValues.PLAY_PAUSE,
+        max_value=RemoteKeyValues.PLAY_PAUSE,
     )
+
+    valid_vals = clamp_enum_to_char(RemoteKeyValues, char)
+
     assert valid_vals == {RemoteKeyValues.PLAY_PAUSE}
