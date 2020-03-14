be2c4b47a69de3377dde7bf721b7cb0dcf45fc59
John Carr <john.carr@unrouted.co.uk>
2020-03-14 13:12:26 +0000

    Update release tooling

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 
-poetry version patch
+VERSION=${1:-patch}
+
+poetry version $VERSION
 git commit -a -m "Version bump"
 git tag -a -s `poetry version | awk '{ print $2; }'`
 git push --tags
