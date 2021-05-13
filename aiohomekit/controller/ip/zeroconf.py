2b512d8f2ca74ca3746b118e1cda86c0719d15ef
J. Nick Koston <nick@koston.org>
2021-05-13 04:56:43 -1000

    isort

--- aiohomekit/controller/ip/zeroconf.py
+++ aiohomekit/controller/ip/zeroconf.py
@@ -25,4 +25,7 @@ with zeroconf.
 This also means we don't need to add any extra dependencies.
 """
 
-from aiohomekit.zeroconf import async_discover_homekit_devices, discover_homekit_devices  # noqa: F401
+from aiohomekit.zeroconf import (  # noqa: F401
+    async_discover_homekit_devices,
+    discover_homekit_devices,
+)
