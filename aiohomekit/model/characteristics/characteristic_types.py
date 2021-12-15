5add557463a65a6ba557f76edbcefa971e63f662
ecoen66 <ecoen@yahoo.com>
2021-12-15 12:42:07 -0600

    some more connectsense

--- aiohomekit/model/characteristics/characteristic_types.py
+++ aiohomekit/model/characteristics/characteristic_types.py
@@ -252,12 +252,16 @@ class _CharacteristicsTypes:
         ECOBEE_FAN_READ_SPEED = "48F62AEC-4171-4B4A-8F0E-1EEB6708B3FB"
 
         # ConnectSense
+        # r/o, float - amps between 0 and 20
+        CONNECTSENSE_ENERGY_AMPERE = "00000004-0000-1000-8000-001D4B474349"
         # r/o, uint32 - state timer - in epoch format
         CONNECTSENSE_STATE_TIMER = "00000005-0000-1000-8000-001D4B474349"
+        # r/o, unit32 - total amps
+        CONNECTSENSE_TOTAL_AMPS = "00000006-0000-1000-8000-001D4B474349"
         # r/o, float - volts between 0 and 130
         CONNECTSENSE_ENERGY_VOLTAGE = "00000008-0000-1000-8000-001D4B474349"
-        # r/o, float - amps between 0 and 20
-        CONNECTSENSE_ENERGY_AMPERE = "00000009-0000-1000-8000-001D4B474349"
+        # r/o, float - amps between 0 and 20 on 20a in-wall outlet
+        CONNECTSENSE_ENERGY_AMPERE_20 = "00000009-0000-1000-8000-001D4B474349"
         # r/o, float - watts
         CONNECTSENSE_ENERGY_WATT = "0000000A-0000-1000-8000-001D4B474349"
         # r/o, int - power factor between 0 and 100
