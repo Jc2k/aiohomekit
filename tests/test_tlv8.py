0bcb9138de20b0e3aa4a9345512dfcac10860900
Josh Roys <roysjosh@gmail.com>
2022-02-01 17:45:52 -0500

    Add tests

--- tests/test_tlv8.py
+++ tests/test_tlv8.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from enum import IntEnum
 
-from aiohomekit.tlv8 import TLVStruct, tlv_entry, u8
+from aiohomekit.tlv8 import TLVStruct, tlv_entry, u8, u64, u128
 
 
 def test_example_1():
@@ -62,3 +62,53 @@ def test_int_enum():
 
     assert DummyStruct(foo=FooValues.ACTIVE).encode() == b"\x01\x01\x01"
     assert DummyStruct(foo=FooValues.INACTIVE).encode() == b"\x01\x01\x00"
+
+
+def test_int_64b():
+    @dataclass
+    class DummyStruct(TLVStruct):
+        prefix: u64 = tlv_entry(48)
+
+    raw = b"\x30\x08" + b"\xbb\xe8\xec<wS\xc8\xfd"
+
+    result = DummyStruct.decode(raw)
+
+    assert f"{result.prefix:X}" == "FDC853773CECE8BB"
+
+    assert result.encode() == raw
+
+
+def test_int_uuid():
+    @dataclass
+    class DummyStruct(TLVStruct):
+        dummy_type: u128 = tlv_entry(16)
+
+    raw = b"\x10\x10" + b"\x83\xc3*\xa6yS\xb1N\x90\xdetZ\xb4l:&"
+
+    result = DummyStruct.decode(raw)
+
+    assert f"{result.dummy_type:X}" == "263A6CB45A74DE904EB15379A62AC383"
+
+    assert result.encode() == raw
+
+
+def test_bytes():
+    @dataclass
+    class DummyStruct(TLVStruct):
+        value: bytes = tlv_entry(160)
+
+    raw = (
+        b"\xA0\x12"
+        + b"\x88\x01\x00\x34"
+        + b"\x00\x01\x00\x05\x74\x68\x2f\x74\x63"
+        + b"\x00\x03\x00\x27\x00"
+    )
+
+    result = DummyStruct.decode(raw)
+
+    assert (
+        result.value
+        == b"\x88\x01\x00\x34\x00\x01\x00\x05\x74\x68\x2f\x74\x63\x00\x03\x00\x27\x00"
+    )
+
+    assert result.encode() == raw
