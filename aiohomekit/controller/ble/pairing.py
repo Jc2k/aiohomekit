9512b0899b999fe51fca7a7c332f76b4e1152013
J. Nick Koston <nick@koston.org>
2022-07-15 18:34:11 -0500

    fix magic number

--- aiohomekit/controller/ble/pairing.py
+++ aiohomekit/controller/ble/pairing.py
@@ -532,7 +532,7 @@ class BlePairing(AbstractPairing):
                 payload = TLV.encode_list(
                     [
                         (HAP_TLV.kTLVHAPParamValue, to_bytes(char, value)),
-                        (HAP_TLV.kTLVHAPParamTTL, b"\x50"),
+                        (HAP_TLV.kTLVHAPParamTTL, b"\x32"),  # 5s
                     ]
                 )
                 response = await self._async_request(
