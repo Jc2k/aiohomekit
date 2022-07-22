cf8f225133f5696f39081265a8de5a369cfc75ea
J. Nick Koston <nick@koston.org>
2022-07-22 12:45:49 -0500

    Offload ble connecting to bleak_retry_connector

--- aiohomekit/controller/ble/connection.py
+++ aiohomekit/controller/ble/connection.py
@@ -16,29 +16,20 @@
 
 from __future__ import annotations
 
-import asyncio
 from collections.abc import Callable
-import logging
 
 from bleak.backends.device import BLEDevice
+from bleak_retry_connector import (
+    BleakConnectionError,
+    BleakNotFoundError,
+    establish_connection as retry_establish_connection,
+)
 
 from aiohomekit.exceptions import AccessoryDisconnectedError, AccessoryNotFoundError
 
-from .bleak import BLEAK_EXCEPTIONS, AIOHomeKitBleakClient
-
-logger = logging.getLogger(__name__)
-
-MAX_TRANSIENT_ERRORS = 9
-
-# Shorter time outs and more attempts
-# seems to be better for dbus, and corebluetooth
-# is happy either way. Ideally we want everything
-# to finish in < 60s or declare we cannot connect
+from .bleak import AIOHomeKitBleakClient
 
 MAX_CONNECT_ATTEMPTS = 5
-BLEAK_TIMEOUT = 10
-
-TRANSIENT_ERRORS = {"le-connection-abort-by-local", "br-connection-canceled"}
 
 
 async def establish_connection(
@@ -48,48 +39,15 @@ async def establish_connection(
     max_attempts: int = MAX_CONNECT_ATTEMPTS,
 ) -> AIOHomeKitBleakClient:
     """Establish a connection to the accessory."""
-    timeouts = 0
-    connect_errors = 0
-    transient_errors = 0
-    attempt = 0
-
-    client = AIOHomeKitBleakClient(device)
-    client.set_disconnected_callback(disconnected_callback)
-
-    def _raise_if_needed(name: str, exc: Exception) -> None:
-        """Raise if we reach the max attempts."""
-        if (
-            timeouts + connect_errors < max_attempts
-            and transient_errors < MAX_TRANSIENT_ERRORS
-        ):
-            return
-        msg = f"{name}: Failed to connect: {exc}"
-        # Sure would be nice if bleak gave us typed exceptions
-        if isinstance(exc, asyncio.TimeoutError) or "not found" in str(exc):
-            raise AccessoryNotFoundError(msg) from exc
-        raise AccessoryDisconnectedError(msg) from exc
-
-    while True:
-        attempt += 1
-        logger.debug("%s: Connecting (attempt: %s)", name, attempt)
-        try:
-            await client.connect(timeout=BLEAK_TIMEOUT)
-        except asyncio.TimeoutError as exc:
-            timeouts += 1
-            logger.debug("%s: Timed out trying to connect (attempt: %s)", name, attempt)
-            _raise_if_needed(name, exc)
-        except BLEAK_EXCEPTIONS as exc:
-            bleak_error = str(exc)
-            if any(error in bleak_error for error in TRANSIENT_ERRORS):
-                transient_errors += 1
-            else:
-                connect_errors += 1
-            logger.debug(
-                "%s: Failed to connect: %s (attempt: %s)", name, str(exc), attempt
-            )
-            _raise_if_needed(name, exc)
-        else:
-            logger.debug("%s: Connected (attempt: %s)", name, attempt)
-            return client
-
-    raise RuntimeError("This should never happen")
+    try:
+        return await retry_establish_connection(
+            AIOHomeKitBleakClient,
+            device,
+            name,
+            disconnected_callback,
+            max_attempts=max_attempts,
+        )
+    except BleakConnectionError as ex:
+        raise AccessoryDisconnectedError(ex) from ex
+    except BleakNotFoundError as ex:
+        raise AccessoryNotFoundError(ex) from ex
