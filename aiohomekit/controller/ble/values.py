4dd24826ba05594b8c87c04dfea73d686abf22bc
J. Nick Koston <nick@koston.org>
2022-10-21 21:00:30 -0500

    floats are 64bit

--- aiohomekit/controller/ble/values.py
+++ aiohomekit/controller/ble/values.py
@@ -27,7 +27,8 @@ def from_bytes(char: Characteristic, value: bytes) -> bool | str | float | int |
     elif char.format == CharacteristicFormats.int:
         return struct.unpack_from("i", value)[0]
     elif char.format == CharacteristicFormats.float:
-        return struct.unpack_from("f", value)[0]
+        # HAP spec says float is 64 bit so its actually a double
+        return struct.unpack_from("d", value)[0]
     elif char.format == CharacteristicFormats.string:
         return value.decode("utf-8")
 
@@ -48,7 +49,8 @@ def to_bytes(char: Characteristic, value: bool | str | float | int | bytes) -> b
     elif char.format == CharacteristicFormats.int:
         value = struct.pack("i", value)
     elif char.format == CharacteristicFormats.float:
-        value = struct.pack("f", value)
+        # HAP spec says float is 64 bit so its actually a double
+        value = struct.pack("d", value)
     elif char.format == CharacteristicFormats.string:
         value = value.encode("utf-8")
 
