60c58f4371e89708045672a5413fb4917eb1f330
J. Nick Koston <nick@koston.org>
2022-12-03 10:43:21 -1000

    cleanup check

--- aiohomekit/controller/ble/pairing.py
+++ aiohomekit/controller/ble/pairing.py
@@ -270,7 +270,7 @@ class BlePairing(AbstractPairing):
 
         if not description and self.state_num:
             self.description = HomeKitAdvertisement.from_cache(
-                name=self.name,
+                name=self.address,
                 address=self.address,
                 id=self.id,
                 config_num=self.config_num,
