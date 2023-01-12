a49a43df2d3f4c31d05a04190b0ebe0f80f27079
Sven Liebehentze <sven.liebehentze@iee.fraunhofer.de>
2023-01-12 13:47:34 +0100

    Handling response as dict and list in case of failure in IpPairing#put_characteristics

--- aiohomekit/controller/ip/pairing.py
+++ aiohomekit/controller/ip/pairing.py
@@ -299,6 +299,8 @@ class IpPairing(ZeroconfPairing):
             # If there is a response it means something failed so
             # we need to remove the listener update for the failed
             # characteristics.
+            if isinstance(response, dict) and "characteristics" in response:
+                response = response["characteristics"]
             for characteristic in response:
                 aid, iid = characteristic["aid"], characteristic["iid"]
                 key = (aid, iid)
