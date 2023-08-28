6dd89cd8b973795f6c0967b489071af441130fd7
J. Nick Koston <nick@koston.org>
2023-08-27 20:08:39 -0500

    Bump minimum python version to 3.11

--- aiohomekit/utils.py
+++ aiohomekit/utils.py
@@ -17,11 +17,7 @@ _LOGGER = logging.getLogger(__name__)
 
 T = TypeVar("T")
 
-if sys.version_info[:2] < (3, 11):
-    from async_timeout import timeout as asyncio_timeout  # noqa: F401
-else:
-    from asyncio import timeout as asyncio_timeout  # noqa: F401
-
+from asyncio import timeout as asyncio_timeout  # noqa: F401
 
 def async_create_task(coroutine: Awaitable[T], *, name=None) -> asyncio.Task[T]:
     """Wrapper for asyncio.create_task that logs errors."""
