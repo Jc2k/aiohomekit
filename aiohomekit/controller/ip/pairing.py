b77f798305f3cb479751e05e5434105ba7bded0d
Bertrand Martel <matsuo@free.fr>
2025-03-22 16:49:44 +0100

    Fix _update_subscriptions - checking characteristic's aid and iid existence
    Somfy TaHoma switch homekit controller /characteristics response is: {'status': 0}
    As consequence, KeyError: 'aid' is raised breaking characteristics subscriptions (poll works, push/ev does not).
    Fix checks for existence of aid and iid.
    Once fix is applied, push/event works perfectly on Somfy TaHoma switch.

--- aiohomekit/controller/ip/pairing.py
+++ aiohomekit/controller/ip/pairing.py
@@ -397,11 +397,12 @@ class IpPairing(ZeroconfPairing):
             if response:
                 # An empty body is a success response
                 for row in response.get("characteristics", []):
-                    status[(row["aid"], row["iid"])] = {
-                        "status": row["status"],
-                        "description": to_status_code(row["status"]).description,
-                    }
-
+                    if "aid" in row and "iid" in row:
+                      status[(row["aid"], row["iid"])] = {
+                          "status": row["status"],
+                          "description": to_status_code(row["status"]).description,
+                      }
+        
         return status
 
     async def async_populate_accessories_state(
